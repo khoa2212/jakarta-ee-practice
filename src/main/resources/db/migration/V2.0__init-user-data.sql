247ebe144f21ccca8e6e88ee440bd5ee28c6db59
Nguyen Tuan Khoa <nguyenkhoa438@gmail.com>
2024-07-12 19:59:34 +0700

    fix migrate file and docker

--- src/main/resources/db/migration/V2.0__init-user-data.sql
+++ src/main/resources/db/migration/V2.0__init-user-data.sql
@@ -1,2 +1,2 @@
-insert into users (email, password, display_name) values ('nguyenkhoa123@gmail.com', '$2a$10$FrYaLZHmfnXqrbL/pSvJiu47uzeVRYcF9doy8OSB0nfvUA1bXwczm', 'Nguyen Tuan Khoa');
-insert into users (email, password, display_name) values ('user@example.com', '$2a$10$FrYaLZHmfnXqrbL/pSvJiu47uzeVRYcF9doy8OSB0nfvUA1bXwczm','User');
+insert into users (email, password, display_name, role) values ('nguyenkhoa123@gmail.com', '$2a$10$FrYaLZHmfnXqrbL/pSvJiu47uzeVRYcF9doy8OSB0nfvUA1bXwczm', 'Nguyen Tuan Khoa', 'ADMIN');
+insert into users (email, password, display_name) values ('phuong@example.com', '$2a$10$FrYaLZHmfnXqrbL/pSvJiu47uzeVRYcF9doy8OSB0nfvUA1bXwczm','Phuong Nguyen');
